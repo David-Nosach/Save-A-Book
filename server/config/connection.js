33d69b0aca153e13958d313647baa15ad14d8cc4
David-Nosach <91391933+David-Nosach@users.noreply.github.com>
2024-07-12 16:49:02 -0700

    added new IP address in atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 
+// Use the MONGODB_URI environment variable if available, otherwise fallback to a local MongoDB instance
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
 );
 
 module.exports = mongoose.connection;
